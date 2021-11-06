21cf5daa066ae6811a4fd602eea99f03dfca7d30
nlanners <lannersn@oregonstate.edu>
2021-11-05 22:53:37 -0600

    async web scraping and rendering accomplished. TODO: connect to image service, fix styling, create loading modal

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const ejs = require('express-ejs-layouts');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json({extended:false}));
@@ -12,60 +14,153 @@ app.use(ejs);
 app.set('port', 65535);
 app.set('view engine', 'ejs');
 
+const imageURL = 'http://jaclynsimagescraper.herokuapp.com/'
+const website = 'https://www.foodnetwork.com/search/'
+
+async function performSearch(searchURL) {
+    // scrape and process
+    try{
+        const response = await axios(searchURL);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        // get titles and urls for search results
+        let searchResults = $('.o-RecipeResult', html);
+        return await buildRecipeList(10, searchResults, $)
+
+        // console.log('performSearch:');
+        // console.log(recipes);
+
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function buildRecipeList(number, searchResults, $) {
+    let recipes = [];
+    for (let i = 0; i < number; i++) {
+        const title = $(searchResults[i]).find('.m-MediaBlock__a-HeadlineText').text().trim();
+        let url = $(searchResults[i]).find('a').attr('href');
+        let selector = title.replace(/ /g, '');
+        selector = selector.replace(/,/g, '');
+        selector = selector.replace(/'/g, '');
+
+        console.log(i + ' : ' + title + ' : ' + url);
+
+        if (url) {
+            url = 'https:' + url;
+            let recipe = await getRecipe(url, title, selector)
+
+            // console.log(recipe);
+            recipes.push(recipe);
+        }
+    }
+    return recipes;
+}
+
+async function getRecipe(url, title, selector) {
+
+    // get data for recipe
+    try {
+        const response = await axios(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const ingredients = $('.o-Ingredients__a-Ingredient', html);
+        const directions = $('.o-Method__m-Step', html);
+        let ingredientList = [];
+        let directionList = []
+
+        for (let i = 1; i < ingredients.length; i++) {
+            ingredientList.push($(ingredients[i]).text().trim());
+        }
+
+        for (let j = 0; j < directions.length; j++) {
+            directionList.push($(directions[j]).text().trim());
+        }
+
+        return {
+            title,
+            url,
+            selector,
+            ingredientList,
+            directionList
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 // Home Page
 app.get('/', function(req,res){
-    res.render('home.ejs', {recipeTitle: null, searchHistory: null});
+    res.render('home.ejs', {recipeTitles: null, searchHistory: null});
 })
 
 app.post('/', function(req,res){
     let context = {};
-    context.recipeTitle = [];
     context.searchHistory = [];
 
     // placeholder for MVP and testing
     let ing1 = req.body.ingredient1;
     let ing2 = req.body.ingredient2;
     let ing3 = req.body.ingredient3;
-    let search = ing1 + '-' + ing2 + '-' + ing3;
 
-    // placeholder for MVP and testing
-    if (ing1) {
-        let recipe = ing1 + '-recipe';
-        context.recipeTitle.push(recipe);
-        if (ing2) {
-            recipe = ing1 + '-' + ing2 + '-recipe';
-            context.recipeTitle.push(recipe);
-            if (ing3) {
-                recipe = ing1 + '-' + ing2 + '-' + ing3 + '-recipe';
-                context.recipeTitle.push(recipe);
-            }
-        }
-    } else if (ing2) {
-        let recipe = ing2 + '-recipe';
-        context.recipeTitle.push(recipe);
-        if (ing3) {
-            recipe = ing2 + '-' + ing3 + '-recipe';
-            context.recipeTitle.push(recipe);
-        }
-    } else if (ing3) {
-        let recipe = ing3 + '-recipe';
-        context.recipeTitle.push(recipe);
-    }
+    let searchURL = website;
 
     // search history builder
-    console.log(req.body.searchHistoryInput);
     let sH = req.body.searchHistoryInput;
 
     searchHistory = sH.split(';');
     if (searchHistory.length >= 10) {
         searchHistory.shift();
     }
-    context.searchHistory = searchHistory.filter(word => word.length > 0);
-    context.searchHistory.push(search);
-    console.log("search history");
-    console.log(context.searchHistory);
 
-    res.render('home.ejs', context);
+    let filteredSH = searchHistory.filter(word => word.length > 0);
+
+    filteredSH.forEach(item => {
+        let ings = item.split('-');
+        context.searchHistory.push({
+            'ing1'  :   ings[0],
+            'ing2'  :   ings[1],
+            'ing3'  :   ings[2],
+            'combinedSearch'    :   item
+        })
+    })
+
+    context.searchHistory.push({
+        'ing1'  :   ing1,
+        'ing2'  :   ing2,
+        'ing3'  :   ing3,
+        'combinedSearch'    :   ing1 + '-' + ing2 + '-' + ing3
+    });
+
+    // set up scrape search
+    if (ing1) {
+        searchURL = searchURL.concat(ing1, '-');
+    }
+    if (ing2) {
+        searchURL = searchURL.concat(ing2, '-');
+    }
+    if (ing3) {
+        searchURL = searchURL.concat(ing3, '-');
+    }
+
+    searchURL = searchURL.concat('recipe-');
+
+    performSearch(searchURL)
+        .then( recipes => {
+            context.recipeTitles = recipes;
+            // console.log('JUST BEFORE RENDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+            // console.log('context: ');
+            // console.log(context);
+            res.render('home.ejs', context);
+        })
+        .catch( (err) => {
+            console.log(err);
+        })
+
+
+
 })
 
 // 404 ERROR
--- public/js/home.js
+++ public/js/home.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', setHistory);
 
 function setHistory() {
-    let historyInput = document.getElementById('searchHistoryInput');
+    let historyInput = document.getElementsByClassName('searchHistoryInput');
     let history = document.getElementsByClassName('historyItem');
 
     console.log('loaded');
     console.log(history);
 
     if (history.length > 0) {
-        var historyList = history[0].innerText + ";";
+        let historyList = history[0].value + ";";
 
         for(let i = 1; i < history.length; i++) {
-            historyList = historyList.concat(history[i].innerText, ";");
-            console.log("history list: " + historyList);
-        }
+            historyList = historyList.concat(history[i].value, ";");
 
-        historyInput.value = historyList;
+        }
+        for(let j = 0; j < historyInput.length; j++) {
+            historyInput[j].value = historyList;
+        }
     }
 
 }
